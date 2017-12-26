89862e23c729584add4c781eebd9f5f97b71d4f7
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2017-12-26 19:00:27 -0300

    add custom changes

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+;(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (package-initialize)
 
@@ -10,8 +10,9 @@
   '(defun enriched-decode-display-prop (start end &optional param)
      (list start end)))
 
+
 ;; No splash screen please ... jeez
-(setq inhibit-startup-message t)
+;(setq inhibit-startup-message t)
 
 ;; Set path to dependencies
 (setq site-lisp-dir
@@ -24,17 +25,13 @@
 (add-to-list 'load-path settings-dir)
 (add-to-list 'load-path site-lisp-dir)
 
-;; Keep emacs Custom-settings in separate file
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
-
 ;; Set up appearance early
 (require 'appearance)
 
 ;; Settings for currently logged in user
-(setq user-settings-dir
-      (concat user-emacs-directory "users/" user-login-name))
-(add-to-list 'load-path user-settings-dir)
+;(setq user-settings-dir
+;      (concat user-emacs-directory "users/" user-login-name))
+;(add-to-list 'load-path user-settings-dir)
 
 ;; Add external projects to load path
 (dolist (project (directory-files site-lisp-dir t "\\w+"))
@@ -60,7 +57,7 @@
 ;; Setup packages
 (require 'setup-package)
 
-;; Install extensions if they're missing
+;;; Install extensions if they're missing
 (defun init--install-packages ()
   (packages-install
    '(magit
@@ -70,39 +67,63 @@
      hydra
      paredit
      move-text
-     gist
      htmlize
      visual-regexp
      markdown-mode
      fill-column-indicator
+     ;auto complete
+     auto-complete
+     deferred
+     company
      flycheck
      flycheck-pos-tip
-     flycheck-clojure
+     ; python
+     jedi
+     python-environment
+     flycheck-pyflakes
+     flycheck-pycheckers
+     ; lua
+     lua-mode
+     luarocks
+     company-lua
+     ; ruby
+     ruby-mode
+     ruby-tools
+     ruby-additional
+     rubocop
+     ; go
+     go
+     go-mode
+     golint
+     go-tag
+     go-impl
+     go-gopath
+     go-add-tags
+     go-gen-test
+     go-snippets
+     go-errcheck
+     go-autocomplete
+     go-eldoc
      flx
      f
      flx-ido
      dired-details
-     css-eldoc
      yasnippet
      smartparens
      ido-vertical-mode
      ido-at-point
-     simple-httpd
      guide-key
-     nodejs-repl
      restclient
      highlight-escape-sequences
      whitespace-cleanup-mode
      elisp-slime-nav
      dockerfile-mode
-     clojure-mode
-     clojure-mode-extra-font-locking
-     groovy-mode
      prodigy
      cider
      yesql-ghosts
      string-edit
      beginend
+     ample-zen-theme
      )))
 
 (condition-case nil
@@ -114,19 +135,23 @@
 ;; Lets start with a smattering of sanity
 (require 'sane-defaults)
 
-;; Setup environment variables from the user's shell.
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;;; Setup environment variables from the user's shell.
 (when is-mac
   (require-package 'exec-path-from-shell)
   (exec-path-from-shell-initialize))
 
-;; guide-key
-(require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +"))
-(guide-key-mode 1)
-(setq guide-key/recursive-key-sequence-flag t)
-(setq guide-key/popup-window-position 'bottom)
-
-;; Setup extensions
+;;; guide-key
+;(require 'guide-key)
+;(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +"))
+;(guide-key-mode 1)
+;(setq guide-key/recursive-key-sequence-flag t)
+;(setq guide-key/popup-window-position 'bottom)
+;
+;;; Setup extensions
 (eval-after-load 'ido '(require 'setup-ido))
 (eval-after-load 'org '(require 'setup-org))
 (eval-after-load 'dired '(require 'setup-dired))
@@ -135,7 +160,7 @@
 (require 'setup-rgrep)
 (require 'setup-hippie)
 (require 'setup-yasnippet)
-(require 'setup-perspective)
+;(require 'setup-perspective)
 (require 'setup-ffip)
 (require 'setup-html-mode)
 (require 'setup-paredit)
@@ -150,20 +175,19 @@
 ;; Default setup of smartparens
 (require 'smartparens-config)
 (setq sp-autoescape-string-quote nil)
-(--each '(css-mode-hook
-          restclient-mode-hook
-          js-mode-hook
+(--each '(restclient-mode-hook
           java-mode
           ruby-mode
+          go-mode
           markdown-mode
-          groovy-mode
+ ;         groovy-mode
           scala-mode)
   (add-hook it 'turn-on-smartparens-mode))
 
 ;; Language specific setup files
 (eval-after-load 'js2-mode '(require 'setup-js2-mode))
 (eval-after-load 'ruby-mode '(require 'setup-ruby-mode))
-(eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
+(eval-after-load 'go-mode '(require 'setup-go-mode))
 (eval-after-load 'markdown-mode '(require 'setup-markdown-mode))
 
 ;; Load stuff on demand
@@ -199,7 +223,7 @@
 (require 'smart-forward)
 (require 'change-inner)
 (require 'multifiles)
-
+;
 ;; Don't use expand-region fast keys
 (setq expand-region-fast-keys-enabled nil)
 
@@ -218,19 +242,19 @@
 (require 'smex)
 (smex-initialize)
 
-;; Setup key bindings
-(require 'key-bindings)
-
-;; Misc
-(require 'project-archetypes)
-(require 'my-misc)
-(when is-mac (require 'mac))
-
+;;; Setup key bindings
+;(require 'key-bindings)
+;
+;;; Misc
+;(require 'project-archetypes)
+;(require 'my-misc)
+;(when is-mac (require 'mac))
+;
 ;; Elisp go-to-definition with M-. and back again with M-,
 (autoload 'elisp-slime-nav-mode "elisp-slime-nav")
 (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t) (eldoc-mode 1)))
 
-;; Emacs server
+; Emacs server
 (require 'server)
 (unless (server-running-p)
   (server-start))
@@ -240,6 +264,6 @@
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 
-;; Conclude init by setting up specifics for the current user
-(when (file-exists-p user-settings-dir)
-  (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
+;;; Conclude init by setting up specifics for the current user
+;;(when (file-exists-p user-settings-dir)
+;;  (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
